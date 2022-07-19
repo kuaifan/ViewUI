a8912aba099d59ba869ab82055bd2a84895064df
kuaifan <aipaw@live.cn>
2022-07-19 09:41:36 +0800

    modal 内容改成v-if

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-transfer-dom :data-transfer="transfer">
         <transition :name="transitionNames[1]">
-            <div :class="maskClasses" :style="wrapStyles" v-show="visible" v-if="showMask" @click="handleMask"></div>
+            <div v-if="visible && showMask" :class="maskClasses" :style="wrapStyles" @click="handleMask"></div>
         </transition>
         <div :class="wrapClasses" :style="wrapStyles" @click="handleWrapClick">
             <transition :name="transitionNames[0]" @after-leave="animationFinish">
-                <div :class="classes" :style="mainStyles" v-show="visible" @mousedown="handleMousedown">
+                <div v-if="visible" :class="classes" :style="mainStyles" @mousedown="handleMousedown">
                     <div :class="contentClasses" ref="content" :style="contentStyles" @click="handleClickModal">
                         <a :class="[prefixCls + '-close']" v-if="closable" @click="close">
                             <slot name="close">
