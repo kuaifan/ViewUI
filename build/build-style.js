760be9fd416a7cc705750630a2cbbdf9120ce16a
kuaifan <aipaw@live.cn>
2025-05-09 17:35:40 +0800

    定义了 @layer base, utilities;

--- build/build-style.js
+++ build/build-style.js
@@ -3,6 +3,8 @@ const cleanCSS = require('gulp-clean-css');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const fs = require('fs');
+const path = require('path');
 
 // 编译less
 gulp.task('css', function () {
@@ -13,7 +15,21 @@ gulp.task('css', function () {
         }))
         .pipe(cleanCSS())
         .pipe(rename('iview.css'))
-        .pipe(gulp.dest('../dist/styles'));
+        .pipe(gulp.dest('../dist/styles'))
+        .on('end', function() {
+            // 在CSS生成完成后进行注释替换
+            const cssFilePath = path.join(__dirname, '../dist/styles/iview.css');
+            try {
+                // 同步读取文件
+                const data = fs.readFileSync(cssFilePath, 'utf8');
+                // 替换 /*! @replace xxxx */ 为 xxxx
+                const result = data.replace(/\/\*!\s*@replace\s+([^*]+?)\s*\*\//g, '$1');
+                // 同步写入文件
+                fs.writeFileSync(cssFilePath, result, 'utf8');
+            } catch (err) {
+                // 静默处理错误
+            }
+        });
 });
 
 // 拷贝字体文件
